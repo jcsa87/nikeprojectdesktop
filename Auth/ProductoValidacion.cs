3c6461d093c77f1510c15603fc889b1243bae912
jcsa87 <juancruzsenicenacosta@gmail.com>
2025-09-21 17:53:00 -0300

    auth

---
+++ Auth/ProductoValidacion.cs
@@ -0,0 +1,26 @@
+﻿using nikeproject.Models;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace nikeproject.Auth
+{
+    public static class ProductoValidacion
+    {
+        public static bool CodigoValido(string codigo)
+            => !string.IsNullOrWhiteSpace(codigo) && codigo.Length <= 20;
+
+        public static bool NombreValido(string nombre)
+            => !string.IsNullOrWhiteSpace(nombre) && nombre.Length <= 100;
+
+        public static bool DescripcionValida(string descripcion)
+            => string.IsNullOrWhiteSpace(descripcion) || descripcion.Length <= 200;
+
+        public static bool PrecioVentaValido(decimal precio)
+            => precio >= 0;
+
+        public static bool EstadoValido(bool estado)
+            => estado == true || estado == false;
+    }
+}
+ 
---
+++ Auth/VentaValidacion.cs
@@ -0,0 +1,17 @@
+﻿using nikeproject.Models;
+using System.Text.RegularExpressions;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+
+
+namespace nikeproject.Auth
+{
+    public static class VentaValidacion
+    {
+        public static bool NumeroDocumentoValido(string numero)
+            => string.IsNullOrWhiteSpace(numero) || numero.Length <= 20;
+
+        public static bool MontoTotalValido(decimal monto)
+            => monto >= 0;
+    }
+}
+ 
