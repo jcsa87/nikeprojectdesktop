82cab094660278ef69e3512608397532ea1f040c
jcsa87 <juancruzsenicenacosta@gmail.com>
2025-10-25 02:17:04 -0300

    esqueleto básico reportes

--- UserControls/ReportesControl.Designer.cs
+++ UserControls/ReportesControl.Designer.cs
@@ -2,42 +2,163 @@
 {
     partial class ReportesControl
     {
+        /// <summary> 
+        /// Variable del diseñador.
+        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
+        /// <summary> 
+        /// Liberar los recursos que se estén utilizando.
+        /// </summary>
         protected override void Dispose(bool disposing)
         {
             if (disposing && (components != null))
+            {
                 components.Dispose();
+            }
             base.Dispose(disposing);
         }
 
+        #region Código generado por el Diseñador
+
         private void InitializeComponent()
         {
-            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ReportesControl));
-            pbReportes = new PictureBox();
-            ((System.ComponentModel.ISupportInitialize)pbReportes).BeginInit();
-            SuspendLayout();
+            this.lblTitulo = new System.Windows.Forms.Label();
+            this.lblDesde = new System.Windows.Forms.Label();
+            this.lblHasta = new System.Windows.Forms.Label();
+            this.dtpDesde = new System.Windows.Forms.DateTimePicker();
+            this.dtpHasta = new System.Windows.Forms.DateTimePicker();
+            this.lblCategoria = new System.Windows.Forms.Label();
+            this.cbCategoria = new System.Windows.Forms.ComboBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            this.chartVentas = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartProductos = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.chartCategorias = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chartVentas)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartProductos)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCategorias)).BeginInit();
+            this.SuspendLayout();
             // 
-            // pbReportes
+            // lblTitulo
             // 
-            pbReportes.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
-       
-            pbReportes.InitialImage = (Image)resources.GetObject("pbReportes.InitialImage");
-            pbReportes.Location = new Point(0, 0);
-            pbReportes.Name = "pbReportes";
-            pbReportes.Size = new Size(872, 702);
-            pbReportes.SizeMode = PictureBoxSizeMode.StretchImage;
-            pbReportes.TabIndex = 6;
-            pbReportes.TabStop = false;
+            this.lblTitulo.Font = new System.Drawing.Font("Segoe UI", 16F, System.Drawing.FontStyle.Bold);
+            this.lblTitulo.Location = new System.Drawing.Point(20, 10);
+            this.lblTitulo.Name = "lblTitulo";
+            this.lblTitulo.Size = new System.Drawing.Size(300, 40);
+            this.lblTitulo.Text = "Reportes y Estadísticas";
+            // 
+            // lblDesde
+            // 
+            this.lblDesde.AutoSize = true;
+            this.lblDesde.Location = new System.Drawing.Point(25, 65);
+            this.lblDesde.Name = "lblDesde";
+            this.lblDesde.Size = new System.Drawing.Size(45, 15);
+            this.lblDesde.Text = "Desde:";
+            // 
+            // dtpDesde
+            // 
+            this.dtpDesde.Location = new System.Drawing.Point(75, 60);
+            this.dtpDesde.Name = "dtpDesde";
+            this.dtpDesde.Size = new System.Drawing.Size(200, 23);
+            // 
+            // lblHasta
+            // 
+            this.lblHasta.AutoSize = true;
+            this.lblHasta.Location = new System.Drawing.Point(290, 65);
+            this.lblHasta.Name = "lblHasta";
+            this.lblHasta.Size = new System.Drawing.Size(41, 15);
+            this.lblHasta.Text = "Hasta:";
+            // 
+            // dtpHasta
+            // 
+            this.dtpHasta.Location = new System.Drawing.Point(340, 60);
+            this.dtpHasta.Name = "dtpHasta";
+            this.dtpHasta.Size = new System.Drawing.Size(200, 23);
+            // 
+            // lblCategoria
+            // 
+            this.lblCategoria.AutoSize = true;
+            this.lblCategoria.Location = new System.Drawing.Point(560, 65);
+            this.lblCategoria.Name = "lblCategoria";
+            this.lblCategoria.Size = new System.Drawing.Size(64, 15);
+            this.lblCategoria.Text = "Categoría:";
+            // 
+            // cbCategoria
+            // 
+            this.cbCategoria.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cbCategoria.Location = new System.Drawing.Point(630, 60);
+            this.cbCategoria.Name = "cbCategoria";
+            this.cbCategoria.Size = new System.Drawing.Size(180, 23);
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnGenerar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGenerar.ForeColor = System.Drawing.Color.White;
+            this.btnGenerar.Location = new System.Drawing.Point(830, 60);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(120, 25);
+            this.btnGenerar.Text = "Generar Reporte";
+            this.btnGenerar.UseVisualStyleBackColor = false;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // chartVentas
+            // 
+            this.chartVentas.Location = new System.Drawing.Point(25, 110);
+            this.chartVentas.Name = "chartVentas";
+            this.chartVentas.Size = new System.Drawing.Size(420, 250);
+            this.chartVentas.Text = "Ventas Mensuales";
+            // 
+            // chartProductos
+            // 
+            this.chartProductos.Location = new System.Drawing.Point(470, 110);
+            this.chartProductos.Name = "chartProductos";
+            this.chartProductos.Size = new System.Drawing.Size(420, 250);
+            this.chartProductos.Text = "Productos Más Vendidos";
+            // 
+            // chartCategorias
+            // 
+            this.chartCategorias.Location = new System.Drawing.Point(25, 380);
+            this.chartCategorias.Name = "chartCategorias";
+            this.chartCategorias.Size = new System.Drawing.Size(865, 250);
+            this.chartCategorias.Text = "Ingresos por Categoría";
             // 
             // ReportesControl
             // 
-            Controls.Add(pbReportes);
-            Name = "ReportesControl";
-            Size = new Size(875, 702);
-            ((System.ComponentModel.ISupportInitialize)pbReportes).EndInit();
-            ResumeLayout(false);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+            this.BackColor = System.Drawing.Color.White;
+            this.Controls.Add(this.lblTitulo);
+            this.Controls.Add(this.lblDesde);
+            this.Controls.Add(this.lblHasta);
+            this.Controls.Add(this.dtpDesde);
+            this.Controls.Add(this.dtpHasta);
+            this.Controls.Add(this.lblCategoria);
+            this.Controls.Add(this.cbCategoria);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.chartVentas);
+            this.Controls.Add(this.chartProductos);
+            this.Controls.Add(this.chartCategorias);
+            this.Name = "ReportesControl";
+            this.Size = new System.Drawing.Size(920, 650);
+            ((System.ComponentModel.ISupportInitialize)(this.chartVentas)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartProductos)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chartCategorias)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
         }
-        private PictureBox pbReportes;
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTitulo;
+        private System.Windows.Forms.Label lblDesde;
+        private System.Windows.Forms.Label lblHasta;
+        private System.Windows.Forms.Label lblCategoria;
+        private System.Windows.Forms.DateTimePicker dtpDesde;
+        private System.Windows.Forms.DateTimePicker dtpHasta;
+        private System.Windows.Forms.ComboBox cbCategoria;
+        private System.Windows.Forms.Button btnGenerar;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartVentas;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartProductos;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chartCategorias;
     }
 }
